55553440ca4de71ea31808e120072e5a946ab1e0
Jain Kuriakose <kuriakosejain456@gmail.com>
2024-07-27 21:46:26 +0530

    first debug and test completed

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,13 @@
+import TypingTest from "@/components/TypingTest";
+import CustomPhrase from "@/components/CustomPharse";
+
 export default function Home() {
-  return <div></div>;
+  return (
+    <div className="container mx-auto p-4">
+      <>
+        <TypingTest />
+        <CustomPhrase />
+      </>
+    </div>
+  );
 }
